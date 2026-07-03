6a042951bf848c9902500052456943f501aacf3e
Artur Kaliuzhnyi <kaliuart@fel.cvut.cz>
2026-07-03 18:11:14 +0200

    init

---
+++ src/main/java/com/artur/jobaggregator/project/ProjectGetJobsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.artur.jobaggregator.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGetJobsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGetJobsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artur/jobaggregator/project/ProjectGetJobsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.jobaggregator.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGetJobsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
